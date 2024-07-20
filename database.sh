73c42debb38cd9d2decca135a4c691faa512bf1c
ahmedttaarek <ahmedttaarek@gmail.com>
2024-07-20 22:37:07 +0300

    Create database

--- database.sh
+++ database.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+
+create_database() {
+    dbname=$(zenity --entry --title="Create Database" --text="Enter database name:")
+    if [ -n "$dbname" ]; then
+        mkdir "$dbname"
+        zenity --info --title="Database Created" --text="Database $dbname created."
+    else
+        zenity --error --title="Error" --text="No database name provided."
+    fi
+}
+
